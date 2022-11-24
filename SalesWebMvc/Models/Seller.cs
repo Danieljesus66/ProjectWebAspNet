99259f0a33f641b1ab14b4b7003fdcaa5fee9ce6
Daniel Jesus <djesus66@gmail.com>
2022-11-23 21:29:46 -0300

    Other entities and second migration

---
+++ SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BrithDate { get; set; }
+        public double BaseSalary { get; set; }
+
+        public Department Department { get; set; }
+        public ICollection<SalesRecords> Sales { get; set; } = new List<SalesRecords>();
+
+        public Seller()
+        {
+        }
+
+        public Seller(int id, string name, string email, DateTime brithDate, double baseSalary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BrithDate = brithDate;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void AddSales(SalesRecords sr)
+        {
+            Sales.Add(sr);
+        }
+
+        public void RemoveSales(SalesRecords sr)
+        {
+            Sales.Remove(sr);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sales.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
+        }
+    }
+}
